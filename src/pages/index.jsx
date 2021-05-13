ffc8e1daf5df16975c614d36ba9bf02f2a00ad0a
Ema Lorenzo <aemalorenzo@gmail.com>
2021-05-12 21:12:11 -0300

    page overlay completely

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Home = ({ modules }) => {
   return (
     <>
-      <div className='h-full'>
+      <div className='min-h-screen'>
         <section className='flex items-center h-full p-20 pt-0'>
           <div className='flex flex-col max-w-4xl gap-y-4'>
             <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600'>
